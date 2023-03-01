e732272aff6c4f507abb6f167459cb01abf5f454
NX3911666 <2412739949@qq.com>
2023-03-01 15:41:54 +0800

    tabbar

---
+++ pages/cate/cate.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    我是分类
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
